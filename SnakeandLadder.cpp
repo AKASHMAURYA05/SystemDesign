eb2916bc6153725a137fc33bd91d75d8dd3a9b4e
Akash5307 <akash1128355@gmail.com>
2024-08-16 00:10:33 +0530

    Game class completed

--- SnakeandLadder.cpp
+++ SnakeandLadder.cpp
@@ -72,12 +72,60 @@ public:
         return 0;
     }
 };
-
+class Game
+{
+public:
+    Board board;
+    vector<Player> players;
+    vector<vector<int>> snakes;
+    vector<vector<int>> ladders;
+    Game(int boardSize, vector<string> playerNames, vector<vector<int>> s, vector<vector<int>> l) : board(boardSize)
+    {
+        for (int i = 0; i < playerNames.size(); i++)
+        {
+            players.pb(Player(playerNames[i]));
+        }
+        snakes = s;
+        ladders = l;
+        initializeBoard();
+    }
+    void initializeBoard()
+    {
+        for (int i = 0; i < snakes.size(); i++)
+        {
+            board.snakePositions.pb(snakes[i][0]);
+            board.snakePositions.pb(snakes[i][1]);
+        }
+        for (int i = 0; i < snakes.size(); i++)
+        {
+            board.ladderPositions.pb(ladders[i][0]);
+            board.ladderPositions.pb(ladders[i][1]);
+        }
+    }
+    void play()
+    {
+        while (1)
+        {
+            for (int i = 0; i < players.size(); i++)
+            {
+                int roll = players[i].rollDice();
+                cout << players[i].name << " rolled a " << roll << "\n";
+                board.movePlayer(players[i], roll);
+                cout << players[i].name << " is now on square " << players[i].position << "\n";
+                if (board.checkWinner(players[i]))
+                {
+                    return;
+                }
+            }
+        }
+    }
+};
 int main()
 {
     vector<string> playerNames = {"Maurya", "Ajit", "Akash"};
     vector<vector<int>> snakes = {{17, 7, 54, 32}};
     vector<vector<int>> ladders = {{1, 5, 8, 9, 13, 37}};
-   
+    Game game(100, playerNames, snakes, ladders);
+    game.play();
     return 0;
 }
